ff454189578d18534c931ad3d2a196261fb579d9
VaibhavSingh79 <E21CSEU0675@bennett.edu.in>
2022-09-09 15:08:53 +0530

    new commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class BubbleSort{
+    public static void printArray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args){
+        // TODO Auto-generated method stub
+        int arr[] = {7, 8, 3, 1, 2};
+
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = 0; j < arr.length -i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
